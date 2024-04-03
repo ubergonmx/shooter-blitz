7848328609d526753e693fc118f4e2866ed87be0
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-04-03 22:53:34 +0800

    Add lighting and shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { SoftShadows } from "@react-three/drei";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
       <color attach="background" args={["#242424"]} />
+      {/* smoothen the shadow to make them less harsh */}
+      <SoftShadows size={42} />
       <Experience />
     </Canvas>
   );
