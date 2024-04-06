73ca810ae22f859da7e18e220330d4b0df2cc0b5
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-04-07 05:53:17 +0800

    Update App component to include Loader and Leaderboard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
-import { PerformanceMonitor, SoftShadows } from "@react-three/drei";
+import { Loader, PerformanceMonitor, SoftShadows } from "@react-three/drei";
 import { Suspense, useState } from "react";
 import { Physics } from "@react-three/rapier";
+import { Leaderboard } from "./components/Leaderboard";
 
 function App() {
   const [downgradedPerformance, setDowngradedPerformance] = useState(false);
 
   return (
-    <Canvas shadows camera={{ position: [0, 30, 0], fov: 30, near: 2 }}>
-      <color attach="background" args={["#242424"]} />
+    <>
+      <Loader />
+      <Leaderboard />
 
-      <SoftShadows
-        // Smoothen the shadow to make them less harsh
-        size={42}
-      />
+      <Canvas shadows camera={{ position: [0, 30, 0], fov: 30, near: 2 }}>
+        <color attach="background" args={["#242424"]} />
 
-      <PerformanceMonitor
-        // Detect low-end devices
-        onDecline={(fps) => setDowngradedPerformance(true)}
-      />
+        <SoftShadows
+          // Smoothen the shadow to make them less harsh
+          size={42}
+        />
 
-      <Suspense>
-        <Physics>
-          <Experience />
-        </Physics>
-      </Suspense>
+        <PerformanceMonitor
+          // Detect low-end devices
+          onDecline={(fps) => setDowngradedPerformance(true)}
+        />
 
-      {!downgradedPerformance && (
-        // Disable postprocessing on low-end devices
-        <EffectComposer disableNormalPass>
-          <Bloom luminanceThreshold={1} intensity={1} mipmapBlur />
-        </EffectComposer>
-      )}
-    </Canvas>
+        <Suspense>
+          <Physics>
+            <Experience />
+          </Physics>
+        </Suspense>
+
+        {!downgradedPerformance && (
+          // Disable postprocessing on low-end devices
+          <EffectComposer disableNormalPass>
+            <Bloom luminanceThreshold={1} intensity={1} mipmapBlur />
+          </EffectComposer>
+        )}
+      </Canvas>
+    </>
   );
 }
 
