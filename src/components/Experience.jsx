8086718c000d03a333644ab41df43d14ae98c133
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-04-06 08:07:50 +0800

    Add player handler and joystick support for multiplayer

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,35 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Map } from "./Map";
-import { useEffect } from "react";
-import { insertCoin } from "playroomkit";
+import { useEffect, useState } from "react";
+import { insertCoin, onPlayerJoin } from "playroomkit";
+import { Joystick } from "playroomkit/multiplayer.mjs";
 
 export const Experience = () => {
+  const [players, setPlayers] = useState([]);
+
   const start = async () => {
     // Show Playroom UI
     await insertCoin();
-  };
+    
+    // Create a joystick for each player
+    onPlayerJoin((state) => {
+      // Create joystick for current player
+      // Others will only sync their state
+      const joystick = new Joystick(state, {
+        type: "angular",
+        buttons: [{ id: "fire", label: "Fire" }]
+      });
+
+      const newPlayer = {state, joystick};
+      state.setState("health", 100);
+      state.setState("deaths", 0);
+      state.setState("kills", 0);
+      setPlayers((players) => [...players, newPlayer]);
+      state.onQuit(() => {
+        setPlayers((players) => players.filter(player => player !== newPlayer));
+      });
+    }
+  )};
 
   useEffect(() => {
     start();
